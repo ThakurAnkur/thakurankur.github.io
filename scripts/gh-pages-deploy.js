d4349c8ef785012c394bd417bcb8951f3f4c61d0
ThakurAnkur <thakur.ankur049@gmail.com>
2021-04-13 21:46:59 +0530

    Changing deploy script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -5,7 +5,7 @@ const fs = require("fs");
     try {
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         console.log("Building...");
-        await execa("yarn", ["run", "build"]);
+        await execa("npm", ["run", "build"]);
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
@@ -13,13 +13,11 @@ const fs = require("fs");
         console.log("Pushing to gh-pages...");
         await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
         await execa("rm", ["-r", folderName]);
+        await execa("git", ["checkout", "-f", "master"]);
+        await execa("git", ["branch", "-D", "gh-pages"]);
         console.log("Successfully deployed");
     } catch (e) {
         console.log(e.message);
         process.exit(1);
-    } finally {
-        await promises.writeFile(configFilePath, originPublicPath, fileOpts);
-        await execa("git", ["checkout", "-f", "master"]);
-        await execa("git", ["branch", "-D", "gh-pages"]);
     }
 })();
